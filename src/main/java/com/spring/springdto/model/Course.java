2fb75d8762b5c866de198408c946b0d2101de898
Mohamed Gabr <mohamedagabr>
2025-09-05 15:13:04 +0300

    add config file

--- src/main/java/com/spring/springdto/model/Course.java
+++ src/main/java/com/spring/springdto/model/Course.java
@@ -1,19 +1,22 @@
 package com.spring.springdto.model;
 import jakarta.persistence.*;
+import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name="courses")
 
 public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="course_id")
     private int courseId ;
     @Column(name="course_name")
     private String courseName ;
-    @Column(name="course_cost")
-    private Double courseCost ;
+    @Column(name = "course_cost", precision = 10, scale = 2)
+    private BigDecimal courseCost ;
     @Column(name="course_time")
-    private Timestamp courseTime ;
-    @Column(name="student_id")
-    private int studentId;
+    private LocalDateTime courseTime ;
+
 }
